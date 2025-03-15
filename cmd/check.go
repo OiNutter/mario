115a354764d2ab92bd11f110f759c816a65aa210
Will McKenzie <willmckenzie@oinutter.co.uk>
2025-03-15 11:00:39 +0000

    improve output and response parsing on check commadn

--- cmd/check.go
+++ cmd/check.go
@@ -5,13 +5,25 @@ package cmd
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+
+type CheckRequest struct {
+	Targets []string `json:"targets"`
+}
+type CheckResult struct {
+	Status string `json:"status" example:"success" description:"Status of the connection check."`
+	Error  string `json:"error,omitempty" example:"connection refused" description:"Error message if the connection check failed."`
+}
+
+type CheckResponse map[string]CheckResult
 
 // checkCmd represents the check command
 var (
@@ -21,20 +33,20 @@ var (
 		Short: "Run connectivity checks for provided targets from configured datacenters.",
 		Long:  `Runs connectivity checks for provided targets from configured datacenters.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Check connectivity for the following targets:")
-
-			for _, v := range targets {
-				fmt.Println(v)
+			jsonBody, err := json.Marshal(&CheckRequest{
+				Targets: targets,
+			})
+			if err != nil {
+				fmt.Printf("Error marshalling request: %v\n", err)
 			}
-
-			jsonBody := []byte(fmt.Sprintf(`{"targets": %v}`, targets))
-			bodyReader := bytes.NewReader(jsonBody)
+			bodyReader := bytes.NewBuffer(jsonBody)
 
 			for _, v := range viper.GetStringSlice("datacenter") {
-				fmt.Println(v)
 
 				requestURL := fmt.Sprintf("%s/check", v)
 				req, err := http.NewRequest(http.MethodPost, requestURL, bodyReader)
+
+				req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
 				if err != nil {
 					fmt.Printf("Error creating request: %v\n", err)
@@ -52,7 +64,44 @@ var (
 					fmt.Printf("Error reading response: %v\n", err)
 					return
 				}
-				fmt.Printf("Response: %s\n", responseBody)
+
+				var response CheckResponse
+				json.Unmarshal(responseBody, &response)
+
+				var anyFailed bool
+				for _, v := range response {
+					if v.Status != "success" {
+						anyFailed = true
+						break
+					}
+				}
+
+				var c *color.Color
+				if anyFailed {
+					c = color.New(color.FgRed)
+				} else {
+					c = color.New(color.FgGreen)
+				}
+
+				c.Println("\n####################")
+				c.Println("Datacentre:", v)
+
+				for k, v := range response {
+					var d *color.Color
+					if v.Status == "success" {
+						d = color.New(color.FgGreen)
+					} else {
+						d = color.New(color.FgRed)
+					}
+
+					fmt.Println("---")
+					d.Println("Target:", k)
+					d.Println("Status:", v.Status)
+					if v.Error != "" {
+						d.Println("Error:", v.Error)
+					}
+				}
+				c.Println("####################")
 			}
 		},
 	}
